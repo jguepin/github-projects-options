99a12205b09fa9551969f09d8cf163564af2d03f
Thomas Mosmant <thomas.mosmant@gmail.com>
2016-12-18 23:35:24 +0100

    Modal code

--- extension/modal.js
+++ extension/modal.js
@@ -1,12 +1,43 @@
 /* globals ghOptionsObserver */
 
 window.ghOptionsModal = (() => {
-  const setup = () => {
-    const observable = 'issue-card';
+  const spinnerHtml = '<img src=\'https://assets-cdn.github.com/images/spinners/octocat-spinner-64.gif\'>';
 
-    ghOptionsObserver.observe(observable, (card) => {
-      console.log(card);
+  const bind = (card) => {
+    const $overlay = $('#gh-projects-options-modal-overlay');
+    const $modal = $('#gh-projects-options-modal');
+
+    $(card).find('h5 a').on('click', (event) => {
+      if (event.target.href) {
+        event.preventDefault();
+        $modal.html(spinnerHtml);
+
+        $.get(event.target.href, (data) => {
+          $modal.html(data);
+        });
+
+        $overlay.show();
+      }
     });
+  };
+
+  const setup = () => {
+    $('body').prepend(Handlebars.templates['modal']());
+
+    const $overlay = $('#gh-projects-options-modal-overlay');
+    const modal = '#gh-projects-options-modal';
+
+    $overlay.on('click', (event) => {
+      const $target = $(event.target);
+
+      if ($target.closest(modal).length) {
+        return true;
+      } else {
+        $overlay.hide();
+      }
+    });
+
+    ghOptionsObserver.observe('issue-card', bind);
   };
 
   return {
