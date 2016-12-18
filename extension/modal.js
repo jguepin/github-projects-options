06a8b3e3bfd3a4735301f0fff42226d3b567b8c7
Thomas Mosmant <thomas.mosmant@gmail.com>
2016-12-18 21:48:38 +0100

    Use the observer in modal.js

--- extension/modal.js
+++ extension/modal.js
@@ -1,5 +1,12 @@
+/* globals ghOptionsObserver */
+
 window.ghOptionsModal = (() => {
   const setup = () => {
+    const observable = 'issue-card';
+
+    ghOptionsObserver.observe(observable, (card) => {
+      console.log(card);
+    });
   };
 
   return {
