5f09b6da06f40700c543df878de90ec1fdd13240
Thomas Mosmant <thomas.mosmant@gmail.com>
2017-02-16 18:45:00 +0100

    Merge the options pane with the menu pane

--- extension/content.js
+++ extension/content.js
@@ -1,29 +1,15 @@
 /* globals ghOptionsFilters, ghOptionsModal */
 
-function addHeaderOptionsIcon() {
-  const headerButtons = $('div.project-header > div.d-table.mt-1.float-right.f6');
-  headerButtons.prepend(Handlebars.templates['header-options-icon']());
-}
-
 function addProjectsOptionsPane() {
-  const panes = $('div.project-pane');
-  const panesParent = panes.parent();
-  const isFullscreen = window.location.search.match(/fullscreen=true/);
-
-  panesParent.append(Handlebars.templates['projects-options-pane']({ isFullscreen }));
+  const paneMenu = $('div.project-pane-menu');
+  paneMenu.append(Handlebars.templates['projects-options-pane']());
 }
 
 function addButtonsEventListeners() {
-  $('button.gh-projects-options-toggle').on('click', toggleProjectOptionsPane);
   $('#gh-projects-options-add-column-toggle').on('click', toggleAddColumnEvent);
   $('#gh-projects-options-project-details-toggle').on('click', toggleProjectDetailsEvent);
   $('#gh-projects-options-project-settings-toggle').on('click', toggleProjectSettingsEvent);
   $('#gh-projects-options-project-modals-toggle').on('click', toggleProjectModalsEvent);
-}
-
-function toggleProjectOptionsPane() {
-  const pane = $('div.project-pane.gh-projects-options-pane');
-  pane.toggleClass('d-none');
 }
 
 function toggleAddColumnEvent(e) {
@@ -60,7 +46,6 @@ function toggleProjectModalsEvent(e) {
 }
 
 $(document).ready(() => {
-  addHeaderOptionsIcon();
   addProjectsOptionsPane();
   addButtonsEventListeners();
   ghOptionsFilters.setup();
